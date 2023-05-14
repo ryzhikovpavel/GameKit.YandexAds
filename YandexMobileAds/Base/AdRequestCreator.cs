8634331342929c983c60c834fa5b84cb2c784ccc
ryzhikovpavel <ryzhikovpavel@gmail.com>
2023-05-15 03:22:18 +0400

    Update YandexAds to 2.7.0 version

--- YandexMobileAds/Base/AdRequestCreator.cs
+++ YandexMobileAds/Base/AdRequestCreator.cs
@@ -12,8 +12,8 @@ namespace YandexMobileAds.Base
         public AdRequest CreateAdRequest(AdRequest adRequest)
         {
             var parameters = adRequest.Parameters ?? new Dictionary<string, string>();
-            parameters[PluginTypeParameter] = PluginType;
-            parameters[PluginVersionParameter] = MobileAdsPackageInfo.PackageVersion;
+            parameters.Add(PluginTypeParameter, PluginType);
+            parameters.Add(PluginVersionParameter, MobileAdsPackageInfo.PackageVersion);
 
             return new AdRequest.Builder().WithAdRequest(adRequest).WithParameters(parameters).Build();
         }
